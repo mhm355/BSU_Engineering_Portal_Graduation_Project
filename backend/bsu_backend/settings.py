dd45e4e8c1731efa21312e612a2ac7bba58c2c01
Mahmoud Hussein <mahmoud.355.mhm@gmail.com>
2025-12-13 23:21:26 +0200

    fix the admin login issue

--- backend/bsu_backend/settings.py
+++ backend/bsu_backend/settings.py
@@ -136,11 +136,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:5172",
+    "http://127.0.0.1:5172",
 ]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:5172",
+    "http://127.0.0.1:5172",
 ]
 
 # CSRF Settings
